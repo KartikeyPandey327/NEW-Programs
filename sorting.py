734ef4de54a1bba3c1f82a5d26f6b2a2dcce10ab
KartikeyPandey <kartikey272007@gmail.com>
2026-05-09 11:01:02 +0530

    Used to sort in assesnding and desending order

---
+++ sorting.py
@@ -0,0 +1,8 @@
+students=["ram","sham","mohan","sohan","suman","rohan","priya","neha","sneha","priyanka"]
+#to sort the list in ascending order
+students.sort()
+print(students)
+
+#to sort the list in descending order
+students.reverse()
+print(students) 
