edcde966e6fb027e4afe7156cb304317fc997274
KartikeyPandey <kartikey272007@gmail.com>
2026-05-09 11:12:54 +0530

    graphs in python

---
+++ Graph.py
@@ -0,0 +1,6 @@
+import matplotlib.pyplot as plt
+plt.plot([1, 2, 3], [4, 5, 6], 'r--')#'ro' for red circles, 'r--' for red dashed line , bst for blue squares, 'g^' for green triangles
+plt.ylabel("y-axis")
+plt.xlabel("x-axis")
+plt.title("Simple Line Graph")
+plt.show()
