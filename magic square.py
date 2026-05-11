a7948249303f6c48f714de5529aab876c6223264
KartikeyPandey <kartikey272007@gmail.com>
2026-05-11 14:43:48 +0530

    added steps to the algorithum

--- magic square.py
+++ magic square.py
@@ -21,7 +21,7 @@ def magic_square(n):
 
             if j==n:
                 j=0
-
+        
         if magicSquare[i][j]!=0:
             i=i+1
             j=j-2
@@ -35,7 +35,18 @@ def magic_square(n):
                 
 
     for row in magicSquare:
+        #print(row)
         print(" ".join(map(str, row)))
 
             
 magic_square(3)
+
+#Steps
+
+#1) In any magic square, 1 is located at position (n/2, n-1).
+
+#2) Let's say the position of 1 i.e. (n/2, n-1) is (p. q), then next number which is 2 is located at (p-1, q+1) position. Anytime if the calculated row position becomes -1 then make it n-1 and if column position becomes n then make it 0.
+
+#3) If the calculated position already contains a number then decrement the column by 2 and increment the row by 1.
+
+#4) If anytime row position becomes -1 and column becomes n, switch it to (0, n-2).
